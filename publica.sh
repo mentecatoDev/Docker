63de7a9e7bb858166987ba67a337f1f4655f1808
Mentecato <favila@iesromerovargas.com>
2023-04-12 20:44:28 +0200

    Crea publica.sh para local y web

--- publica.sh
+++ publica.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-# Lanzar el programa nav18.py
+# Ejecuta la instrucción python
 python ./auto/nav18.py
 
-# Lanzar mkdocs en local
-mkdocs serve
+# Verifica si se proporcionó un parámetro
+if [ $# -eq 1 ]; then
+  respuesta="$1"
+else
+  # Si no se proporcionó ningún parámetro, pregunta si se desea publicar en la web o en local
+  read -p "¿Deseas publicar en la web (w) o en local (l)? " respuesta
+fi
+
+# Verifica la respuesta dada
+if [ "$respuesta" = "w" ]; then
+  # Si la respuesta es "w", ejecuta mkdocs gh-deploy
+  mkdocs gh-deploy
+  # Abre Firefox con la dirección web correspondiente
+  firefox http://mentecatodev.github.io/docker
+elif [ "$respuesta" = "l" ]; then
+  # Si la respuesta es "l", verifica si el servidor local ya está en funcionamiento
+  if lsof -Pi :8000 -sTCP:LISTEN -t >/dev/null ; then
+    # Si el servidor local ya está en funcionamiento, lo detiene
+    echo "El servidor local ya está en funcionamiento. Lo detendrá y lo volverá a levantar."
+    lsof -t -i :8000 | xargs kill -9 >/dev/null 2>&1
+  fi
+  # Levanta el servidor local
+  mkdocs serve >/dev/null 2>&1 &
+  # Espera 1 segundo para asegurarse de que el servidor esté en marcha
+  sleep 1
+  # Abre Firefox con la dirección local correspondiente
+  firefox http://localhost:8000
+else
+  # Si la respuesta no es "w" ni "l", muestra un mensaje de error
+  echo "Respuesta no válida. Debe ser 'w' para publicar en la web o 'l' para ejecutar en local."
+fi
